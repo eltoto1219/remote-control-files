cbeea4184677b038c80fb87f77b17bcc3f910496
Antonio Mendoza <antonio36764@gmail.com>
2020-02-12 23:11:31 -0500

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #set vimrc path an for neovim
 mkdir -p $HOME/.config/nvim; printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" > $HOME/.config/nvim/init.vim
 
