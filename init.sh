9634f10c65b8484e29dc447e1cb4ae18edecd2f0
eltoto1219 <antonio36764@gmail.com>
2020-11-27 18:58:20 -0500

    added new .bash* file stuff

--- init.sh
+++ init.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 #download node js for the thing
-curl -sL install-node.now.sh | bash
-source $HOME/.bashrc
+#curl -sL install-node.now.sh | bash
+#source $HOME/.bashrc
 
 #get plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#add changes to bash
-echo "if [[ $TERM == xterm ]]; then TERM=xterm-256color; fi" >> $HOME/.bashrc
-echo "set -o vi" >> $HOME/.bashrc
-
 #Run Plug Install
 vim +'PlugInstall' +qa
 vim +'PlugUpdate' +qa
 source ~/.bashrc
+source ~/.bash_profile
 
 vim +'CocInstall coc-json coc-tsserver coc-yaml coc-python coc-css coc-yaml coc-snippets' +qa
 
-# cp $HOME/remote-control-files/setup.cfg $HOME/setup.cfg
-# cp $HOME/remote-control-files/coc-settings.json $HOME/.vim/coc-settings.json
+cp setup.cfg $HOME/setup.cfg
+cp coc-settings.json $HOME/.vim/coc-settings.json
+#cp .bashrc $HOME/.bashrc
+#cp .bash_profile $HOME/.bash_profile
