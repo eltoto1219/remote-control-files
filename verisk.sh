68c8e464da309425776b55f6b3f61d7fc29b61f2
Ubuntu <ubuntu@01d5e2d7cc2c0c447.awsveriskt.local>
2022-02-10 18:42:11 -0500

    fix

--- verisk.sh
+++ verisk.sh
@@ -3,20 +3,21 @@
 sudo chown -R ubuntu /opt
 mkdir /opt/cache
 cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/kvp-copynet.git
+cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/rate-filing-automation.git
 source activate pytorch_latest_p37
-pip install --upgrade pip
-pip install strsim
-pip install transformers
-pip install wandb
-pip install datasets
-pip install jsonlines
-pip install nltk
-pip install numpy
-
+pip install pip-tools
+#pip install --upgrade pip
+#pip install strsim
+#pip install transformers
+#pip install wandb
+#pip install datasets
+#pip install jsonlines
+#pip install nltk
+#pip install numpy
 cd ~/remote-control-files
 
 echo "source activate pytorch_latest_p37" >> ~/.profile
-echo "cd /opt/kvp-copynet" >> ~/.profile
+echo "cd /opt/rate-filing-automation" >> ~/.profile
 echo "set -o vi" >> ~/.bashrc
 echo "PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
