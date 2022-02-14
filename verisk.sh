e7d2c8ba943fa1c7a443ce16b043eb26a16f830a
Antonio Mendoza <antoniomendoza@mac.attlocal.net>
2022-02-14 16:11:43 -0500

    final

--- verisk.sh
+++ verisk.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
+echo "Setting up EC2 Instance"
 sudo chown -R ubuntu /opt
 mkdir /opt/cache
 cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/kvp-copynet.git
 cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/rate-filing-automation.git
 source activate pytorch_latest_p37
 pip install pip-tools
-#pip install --upgrade pip
-#pip install strsim
-#pip install transformers
-#pip install wandb
-#pip install datasets
-#pip install jsonlines
-#pip install nltk
-#pip install numpy
-cd ~/remote-control-files
-
 echo "source activate pytorch_latest_p37" >> ~/.profile
-echo "cd /opt/rate-filing-automation" >> ~/.profile
 echo "set -o vi" >> ~/.bashrc
 echo "PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 echo "force_color_prompt=yes" >> ~/.bashrc
 echo "export WANDB_CACHE_DIR="/opt/cache"" >> ~/.bashrc
+
+echo "Removing unsued environments"
+./rm_envs.sh
+
+echo "Setting up Vim"
+./init.sh
