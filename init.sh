af6b78df87ae19701fcb27c00462dc0a706f716b
Antonio Mendoza <antoniomendoza@mac.attlocal.net>
2022-02-14 16:28:31 -0500

    fix

--- init.sh
+++ init.sh
@@ -33,7 +33,7 @@ cp setup.cfg ~/
 cp .hushlogin ~/
 
 # add to path
-echo "PATH=$PATH:$HOME/.local/bin" >> ~/.profile
+echo "PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 
 # install all neccessary python packages
 pip3 install -r requirements.in
--- verisk.sh
+++ verisk.sh
@@ -7,12 +7,20 @@ cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/kvp-copyn
 cd /opt/ && git clone https://dev.jde.cloud.verisk.com/Antonio.Mendoza/rate-filing-automation.git
 source activate pytorch_latest_p37
 pip install pip-tools
-echo "source activate pytorch_latest_p37" >> ~/.profile
+
+echo "source activate pytorch_latest_p37" >> ~/.bashrc
 echo "set -o vi" >> ~/.bashrc
 echo "PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 echo "force_color_prompt=yes" >> ~/.bashrc
 echo "export WANDB_CACHE_DIR="/opt/cache"" >> ~/.bashrc
+
+echo "source activate pytorch_latest_p37" >> ~/.profile
+echo "set -o vi" >> ~/.profile
+echo "PYTHONDONTWRITEBYTECODE=1" >> ~/.profile
+echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.profile
+echo "force_color_prompt=yes" >> ~/.profile
+echo "export WANDB_CACHE_DIR="/opt/cache"" >> ~/.profile
 
 echo "Removing unsued environments"
 bash $HOME/remote-control-files/rm_envs.sh
